5284e580166a92d3a53c0da1ebb87fb7b5b34ba4
Kevin Susanto <nkevinsst@gmail.com>
2025-08-28 19:57:11 +0800

    Fix migration issue

--- database/migrations/2025_08_21_141634_add_real_flight_operation_to_event_types_table.php
+++ database/migrations/2025_08_21_141634_add_real_flight_operation_to_event_types_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
-class AddRealFlightEventToEventTypesTable extends Migration
+class AddRealFlightOperationToEventTypesTable extends Migration
 {
     /**
      * Run the migrations.

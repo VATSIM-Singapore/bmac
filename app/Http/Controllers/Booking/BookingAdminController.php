53cc79c7f7a51082c1e482c0f0d59aadb57eb65c
Kevin Susanto <nkevinsst@gmail.com>
2025-08-29 15:54:50 +0800

    Fix user reference in booking deletion event trigger, update booking import excel template to include Airline

--- app/Http/Controllers/Booking/BookingAdminController.php
+++ app/Http/Controllers/Booking/BookingAdminController.php
@@ -237,7 +237,7 @@ class BookingAdminController extends AdminController
     public function destroy(Booking $booking): RedirectResponse
     {
         if ($booking->event->endEvent >= now()) {
-            if (!empty($booking->user)) {
+            if (!empty($booking->user_id)) {
                 event(new BookingDeleted($booking->event, $booking->user));
             }
             $booking->delete();
